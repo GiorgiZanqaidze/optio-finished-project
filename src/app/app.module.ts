2df1314a9d90547cc3d3eca6c79267a02a23256e
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-10-07 22:12:18 +0400

    feat: add ngrx store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BannersListComponent } from './banners-list/banners-list.component';
+import {StoreModule} from "@ngrx/store";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannersListComponent } from './banners-list/banners-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
