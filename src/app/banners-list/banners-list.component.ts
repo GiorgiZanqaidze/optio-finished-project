8722fafa7a7a2abae5a982fc066bfa2d8a7d1d64
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-10-08 13:39:01 +0400

    feature: use service to fetch banners data

--- src/app/banners-list/banners-list.component.ts
+++ src/app/banners-list/banners-list.component.ts
@@ -4,6 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {PageEvent} from "@angular/material/paginator";
 import {BannerModel} from "../types/banners/banner.model";
 import {FormControl, FormGroup} from "@angular/forms";
+import {BannersService} from "../services/banners.service";
 
 @Component({
   selector: 'app-banners-list',
@@ -15,6 +16,7 @@ export class BannersListComponent implements OnInit{
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private bannersService: BannersService,
     private http: HttpClient
   ) {
   }
@@ -35,22 +37,28 @@ export class BannersListComponent implements OnInit{
   ngOnInit() {
     this.route.queryParams.subscribe((route :Params) => {
       this.page = +route['page']
-      this.http.post('https://development.api.optio.ai/api/v2/banners/find',
-        {
-          search:this.searchInput,
-          "pageSize": 10,
-          "pageIndex": this.page,
-        },
-        {
-          headers: {
-            accept: "application/json",
-            Authorization: "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY4OTI3LCJleHAiOjE2OTc0MzI5Mjd9.eXt_P4gBxAH6QwUDRpmKixVLn0BgLAq09Kj3F8W8oMJQqid8Sjxi-xVR1PNnrfRCViWuvVHX8D4B6FLzX15X9Q"
-          },
-        }).subscribe((banners : any) => {
-        console.log(banners.data)
-        this.totalPages = banners.data.total
-        this.banners = banners.data.entities
-      })
+      // this.http.post('https://development.api.optio.ai/api/v2/banners/find',
+      //   {
+      //     search:this.searchInput,
+      //     "pageSize": 10,
+      //     "pageIndex": this.page,
+      //   },
+      //   {
+      //     headers: {
+      //       accept: "application/json",
+      //       Authorization: "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY4OTI3LCJleHAiOjE2OTc0MzI5Mjd9.eXt_P4gBxAH6QwUDRpmKixVLn0BgLAq09Kj3F8W8oMJQqid8Sjxi-xVR1PNnrfRCViWuvVHX8D4B6FLzX15X9Q"
+      //     },
+      //   }).subscribe((banners : any) => {
+      //   console.log(banners.data)
+      //   this.totalPages = banners.data.total
+      //   this.banners = banners.data.entities
+      // })
+      this.bannersService.fetchBanners(this.searchInput, this.page)
+        .subscribe((data) => {
+          this.totalPages = data.totalPages
+          this.banners = data.banners
+        })
+
     })
   }
 
