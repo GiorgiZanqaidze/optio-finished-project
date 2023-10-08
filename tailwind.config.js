e937440eb21020a8900534f83d78cd0e407efd10
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-10-08 09:22:55 +0400

    config: tailwind screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px', // Small screen
+        'md': '768px', // Medium screen
+        'lg': '1024px', // Large screen
+      },
+      colors: {
+        primary: '#ff5733',
+        secondary: '#3498db',
+        accent: '#f39c12',
+      },
+    },
   },
   plugins: [],
 }
