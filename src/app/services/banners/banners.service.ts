a424779c00065407413122bfbc2cab2262c09b38
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-10-10 23:07:38 +0400

    refactor: add auth interceptor

--- src/app/services/banners/banners.service.ts
+++ src/app/services/banners/banners.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from '../../../environments/environment'
 
 
 @Injectable({
@@ -7,32 +8,20 @@ import {HttpClient} from "@angular/common/http";
 })
 export class BannersService {
 
+    private readonly ApiBaseUrl: string
+
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+  ) {
+        this.ApiBaseUrl = environment.ApiUrl
+  }
 
   fetchBanners(searchInput: string, page: number) {
-    return this.http.post('https://development.api.optio.ai/api/v2/banners/find',
+    return this.http.post(`${this.ApiBaseUrl}/banners/find`,
       {
         search:searchInput,
         "pageSize": 4,
         "pageIndex": page,
-      },
-      {
-        headers: {
-          accept: "application/json",
-          Authorization: "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY4OTI3LCJleHAiOjE2OTc0MzI5Mjd9.eXt_P4gBxAH6QwUDRpmKixVLn0BgLAq09Kj3F8W8oMJQqid8Sjxi-xVR1PNnrfRCViWuvVHX8D4B6FLzX15X9Q"
-        },
       })
-  }
-
-  getBlob(fileId: any) {
-      return this.http.get(`https://development.api.optio.ai/api/v2/blob/${fileId}`,
-          {
-              headers: {
-                  accept: "application/json",
-                  Authorization: "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY4OTI3LCJleHAiOjE2OTc0MzI5Mjd9.eXt_P4gBxAH6QwUDRpmKixVLn0BgLAq09Kj3F8W8oMJQqid8Sjxi-xVR1PNnrfRCViWuvVHX8D4B6FLzX15X9Q"
-              },
-          })
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    ApiUrl: 'https://development.api.optio.ai/api/v2'
+};
