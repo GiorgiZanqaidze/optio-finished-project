346f44a2deff0b9cdb7f312d6c93807ae6bae051
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-10-10 23:45:51 +0400

    refactor: banners list item

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: true,
-    ApiUrl: 'https://development.api.optio.ai/api/v2'
+    production: false,
+    ApiUrl: 'https://development.api.optio.ai/api/v2',
+    JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY4OTI3LCJleHAiOjE2OTc0MzI5Mjd9.eXt_P4gBxAH6QwUDRpmKixVLn0BgLAq09Kj3F8W8oMJQqid8Sjxi-xVR1PNnrfRCViWuvVHX8D4B6FLzX15X9Q"
 };
