48a039d5df3e6963564169922e1aa5892b9c1ef4
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-10-16 09:54:49 +0400

    change: jwt token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     ApiUrl: "https://development.api.optio.ai/api/v2",
-    JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk2NTY4OTI3LCJleHAiOjE2OTc0MzI5Mjd9.eXt_P4gBxAH6QwUDRpmKixVLn0BgLAq09Kj3F8W8oMJQqid8Sjxi-xVR1PNnrfRCViWuvVHX8D4B6FLzX15X9Q",
+    JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZ2lvcmdpemFucWFpZHplb2ZmaWNpYWxAZ21haWwuY29tIiwiaWF0IjoxNjk3NDMwMjgwLCJleHAiOjE2OTgyOTQyODB9.NLd56CDQqLO-uKNjm-h6J8QvfcJ9sYG7VhwUG8YSUUpJfrmujotZnV6_8v2s2RVwdTYW24wCZJbBg-O408HM4Q",
     channels_type_id: "1600",
     zones_type_id: "1700",
     labels_type_id: "1900",
