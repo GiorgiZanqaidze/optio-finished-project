adf526ecc3c91e1c5dcf154c57219a7a7c3f2edb
giorgizanqaidze <giorgizanqaidzeofficial@gmail.com>
2024-03-31 21:55:39 +0400

    changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   ApiUrl: "https://development.api.optio.ai/api/v2",
-  JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQG9wdGlvLmFpIiwic3ViIjoiYWRtaW4iLCJpYXQiOjE3MDA1NTY2NzUsImV4cCI6MTcwMjI4NDY3NX0.uZpHVzQLpEWTex7sEWxjFce2QGo786vbSuUN3N0qtQXKjLnlw7rQrFkXYzzcAMXbTz29lCw08CYJQL8UWIeY8g",
+  JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQG9wdGlvLmFpIiwic3ViIjoiYWRtaW4iLCJpYXQiOjE3MTE5MDc0NjYsImV4cCI6MTcxMzYzNTQ2Nn0.4-kgCBIeKrylBETjkKovNYEyOAfJCcs5pKNuPpOI5YMyTjRiOkxojbWIoTQkJAR9cn8AHIax72976U52QB80Uw",
   channels_type_id: "1600",
   zones_type_id: "1700",
   labels_type_id: "1900",
