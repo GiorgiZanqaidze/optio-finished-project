1147175f8503157fdcff0e59481af705ee1d50dc
giorgizankaidze <giorgi.zankaidze@optio.ai>
2024-05-16 18:37:36 +0400

    test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   ApiUrl: "https://development.api.optio.ai/api/v2",
-  JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQG9wdGlvLmFpIiwic3ViIjoiYWRtaW4iLCJpYXQiOjE3MTE5MDc0NjYsImV4cCI6MTcxMzYzNTQ2Nn0.4-kgCBIeKrylBETjkKovNYEyOAfJCcs5pKNuPpOI5YMyTjRiOkxojbWIoTQkJAR9cn8AHIax72976U52QB80Uw",
+  JwtToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluQG9wdGlvLmFpIiwic3ViIjoiYWRtaW4iLCJpYXQiOjE3MTU3NzgzNTgsImV4cCI6MTcxNzUwNjM1OH0.ptG6cFMMTsUuO4OdQmi5-Q93GOUkTiFxA0-XPIHfdGnyOtZZU-b--mjCHIGSjahr8jRmAEsCO7FSNvN5v3g6lA",
   channels_type_id: "1600",
   zones_type_id: "1700",
   labels_type_id: "1900",
